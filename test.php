300037022282b24d75c6afb6418e6117cf2a83c8
hakasenyang <contact@hakase.kr>
2016-12-11 04:00:01 +0900

    Edit test file

--- test.php
+++ test.php
@@ -14,6 +14,8 @@
 
     if (empty($Check->CheckPerformance()))
         echo 'Error<br>';
+    echo 'Play Count : ' . $Check->CheckPlayCount();
+    echo '<br>';
     echo 'Performance : ' . $Check->CheckPerformance();
     echo '<br>';
     echo 'Ranking : ' . $Check->CheckRank();
@@ -28,4 +30,12 @@
     else
         echo '<br>Please change occupation to ' . $Check->GetOccupation() . '<br>Your Occupation : ' . $occu;
     echo '<br>';
+
+    if (empty($Check->CheckPerformance()))
+        echo 'Error<br>';
+    echo 'Play Count : ' . $Check->CheckPlayCount();
+    echo '<br>';
     echo 'Performance : ' . $Check->CheckPerformance();
+    echo '<br>';
+    echo 'Ranking : ' . $Check->CheckRank();
+    echo '<br><br>';
