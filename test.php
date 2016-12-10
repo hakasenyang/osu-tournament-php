dcdd6df1e352823b34dbe90f17af8ddc096843c3
hakasenyang <contact@hakase.kr>
2016-12-11 04:07:32 +0900

    legibility

--- test.php
+++ test.php
@@ -2,40 +2,37 @@
     require_once 'include.php';
     $Check = new OsuTournament\Check();
 
+    function GetDataTest()
+    {
+        global $Check;
+        echo $Check->RealID . ' (' . $Check->OsuID . ')';
+        $occu = $Check->CheckOccupation();
+
+        if ($Check->CheckOccupation() === true)
+            echo '<br>You are applied!';
+        elseif (empty($occu))
+            echo '<br>Please change occupation to ' . $Check->GetOccupation();
+        else
+            echo '<br>Please change occupation to ' . $Check->GetOccupation() . '<br>Your Occupation : ' . $occu;
+        echo '<br>';
+
+        if (empty($Check->CheckPerformance()))
+            echo 'Error<br>';
+        echo 'Play Count : ' . $Check->CheckPlayCount();
+        echo '<br>';
+        echo 'Performance : ' . $Check->CheckPerformance();
+        echo '<br>';
+        echo 'Ranking : ' . $Check->CheckRank();
+        echo '<br><br>';
+    }
     $Check->CheckUser(2558286, 0);
-    echo $Check->RealID . ' (' . $Check->OsuID . ')';
-    $occu = $Check->CheckOccupation();
+    GetDataTest();
 
-    if ($Check->CheckOccupation() === true)
-        echo '<br>You are applied!';
-    else
-        echo '<br>Please change occupation to ' . $Check->GetOccupation() . '<br>Your Occupation : ' . $occu;
-    echo '<br>';
+    $Check->CheckUser('angelsIm', 0);
+    GetDataTest();
 
-    if (empty($Check->CheckPerformance()))
-        echo 'Error<br>';
-    echo 'Play Count : ' . $Check->CheckPlayCount();
-    echo '<br>';
-    echo 'Performance : ' . $Check->CheckPerformance();
-    echo '<br>';
-    echo 'Ranking : ' . $Check->CheckRank();
-    echo '<br><br>';
+    $Check->CheckUser('peppy', 0);
+    GetDataTest();
 
-    $Check->CheckUser('Angelsim', 0);
-    echo $Check->RealID . ' (' . $Check->OsuID . ')';
-    $occu = $Check->CheckOccupation();
-
-    if ($Check->CheckOccupation() === true)
-        echo '<br>You are applied!';
-    else
-        echo '<br>Please change occupation to ' . $Check->GetOccupation() . '<br>Your Occupation : ' . $occu;
-    echo '<br>';
-
-    if (empty($Check->CheckPerformance()))
-        echo 'Error<br>';
-    echo 'Play Count : ' . $Check->CheckPlayCount();
-    echo '<br>';
-    echo 'Performance : ' . $Check->CheckPerformance();
-    echo '<br>';
-    echo 'Ranking : ' . $Check->CheckRank();
-    echo '<br><br>';
+    $Check->CheckUser('banchobot', 0);
+    GetDataTest();
