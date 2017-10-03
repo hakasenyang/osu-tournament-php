570a894621164d6a46e13d729145e85c8bd2ce4e
Hakase <hakase@hakase.kr>
2017-10-03 23:22:55 +0900

    include tab 및 소스 정리
    ㄲㄲ

--- include.php
+++ include.php
@@ -3,8 +3,11 @@
  * @file include.php
  * @author Hakase (contact@hakase.kr)
  */
-    spl_autoload_register(function($classname) {
-    	$file = __DIR__.'/class/'.strtolower(str_replace("\\", "/", $classname)).'.php';
-    	if(file_exists($file))
-        	require_once(__DIR__.'/class/'.strtolower(str_replace("\\", "/", $classname)).'.php');
-    });
+    spl_autoload_register(
+        function($classname)
+        {
+            $file = __DIR__.'/class/'.strtolower(str_replace("\\", "/", $classname)).'.php';
+            if (file_exists($file))
+                require_once(__DIR__.'/class/'.strtolower(str_replace("\\", "/", $classname)).'.php');
+        }
+    );
